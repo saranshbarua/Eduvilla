c48acfbc0e9007c892f891d07f324a42caaec774
saranshbarua <saranshbarua@gmail.com>
2017-11-12 14:50:49 +0530

    project stage one

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['Home','firstPage', 'secondPage'],
+		menu: '#navu'
+});
+
+});
+	$('#jumpin').on('click',function(){
+		$('.box').css('display','none');
+		$('.main').css('display','block');
+	});
+
+// function testScroll(ev){
+//   if(window.pageYOffset<270)
+//       {
+//         $('#navu').css('display','none');
+//       }
+//   if(window.pageYOffset>270)
+//       {
+//         $('#navu').css('display','block');
+//         $('#navu').addClass('na');
+//       }
+// }
+// window.onscroll=testScroll;
